d822aa5e73ad8c207e63e362d3233556d4623b7b
Kritsadawut Deemonkhon <108039858+kritsadawut07@users.noreply.github.com>
2022-07-18 23:35:17 +0700

    basic c++

---
+++ lab3_1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main (){
+    int q1 ;
+    float q2 ;
+    char q3 ;
+    printf("please enter value :") ;
+    scanf("%d %f %c",&q1,&q2,&q3) ;
+    printf("%d %.2f %c",q1,q2,q3) ;
+    
+	return 0 ;
+}
